6a5860e246a01e78f812a2eae9e5a281f71a5de2
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-06-14 16:31:47 -0400

    Delete usersprofile.js

--- public/js/usersprofile.js
+++
@@ -1,8 +0,0 @@
-// let signOut = document.getElementById("nav-logout");
-// let aProfile = document.getElementById("nav-profile");
-//
-// signOut.hidden = true;
-// aProfile.hidden = true;
-
-// $('#nav-logout').hide();
-// $('#nav-profile').hide();
