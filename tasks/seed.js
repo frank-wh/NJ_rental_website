6a4193c5b606184f63829662b28d09508b1872a6
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-06-15 22:24:41 -0400

    Add files via upload

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,24 +1,146 @@
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
-bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const users = data.users;
-saltRounds  = 5;
+const houses = data.houses;
+// const comments = data.comments;
+const saltRounds = 5;
 
 async function main() {
-	const db = await dbConnection();
+    const db = await dbConnection();
+	await db.dropDatabase();
+    
+    const user1_pw = await bcrypt.hash('Festive', saltRounds);
+	const user1 = await users.addUser('festive', 'festive@gmail.com', '823-608-2417', user1_pw);
+	const house1 = await houses.addHouse(
+		'313 7th St, Downtown, NJ 07302', 
+		'Downtown Charmer - 17 Mins to NYC!', 
+		user1._id, 40.726246, -74.0481387, 'Entire home/apt', 4100, ''
+	);
 
-	
-	const test_user4_pw = await bcrypt.hash('Arman2', saltRounds);
-	await users.addUser('arman2', 'arman2@gmail.com', '913-607-0188', test_user4_pw);
+	const user2_pw = await bcrypt.hash('Javier', saltRounds);
+	const user2 = await users.addUser('javier', 'javier@gmail.com', '718-680-7937', user2_pw);
+	const house2 = await houses.addHouse(
+		'38 Romaine Ave, Jersey City, NJ 07306', 
+		'Beautiful apt., 10 min subway ride to Manhattan', 
+		user2._id, 40.730931, -74.06998, 'Entire home/apt', 2950, ''
+	);
 
-	// const test_user5_pw = await bcrypt.hash('Gil225', saltRounds);
-	// await users.addUser('gil225', 'gil225@gmail.com', '426-001-4994', test_user5_pw);
-	
-	// const test_user6_pw = await bcrypt.hash('Susan8', saltRounds);
-    // await users.addUser('susan8', 'susan8@gmail.com', '394-666-3799', test_user6_pw);
+    const user3_pw = await bcrypt.hash('Martin', saltRounds);
+	const user3 = await users.addUser('martin', 'martin@gmail.com', '913-607-0188', user3_pw);
+	const house3 = await houses.addHouse(
+		'360 Marin Blvd, Downtown, NJ 07302', 
+		'HUGE PENTHOUSE LUXURY!!', 
+		user3._id, 40.7212416, -74.0407989, 'Entire home/apt', 3520, ''
+	);
+
+	const user4_pw = await bcrypt.hash('Massimo', saltRounds);
+	const user4 = await users.addUser('massimo', 'massimo@gmail.com', '234-440-2896', user4_pw);
+	const house4 = await houses.addHouse(
+		'149 Terhune Ave, Greenville, NJ 07305', 
+		'2 bedroom, minutes from bus stop to journal sq', 
+		user4._id, 40.705586, -74.0969139, 'Entire home/apt', 2000, ''
+	);
+
+	const user5_pw = await bcrypt.hash('Susan8', saltRounds);
+    const user5 = await users.addUser('susan8', 'susan8@gmail.com', '394-566-3799', user5_pw);
+	const house5 = await houses.addHouse(
+		'133 Lafayette St, Bergen Lafayette, NJ 07304', 
+		'Jersey City, 1-Bedroom Garden Apt.', 
+		user5._id, 40.7136919, -74.0633154, 'Entire home/apt', 2370, ''
+	);
+
+	const user6_pw = await bcrypt.hash('Juliet', saltRounds);
+    const user6 = await users.addUser('juliet', 'juliet@gmail.com', '201-573-8675', user6_pw);
+	const house6 = await houses.addHouse(
+		'2091 John F. Kennedy Blvd, North Bergen, NJ 07047', 
+		'BNB by NYC: You own 2Floors & 5 Room...Spread Out!', 
+		user6._id, 40.7686981, -74.0405169, 'Private Room', 900, ''
+	);
+
+    const user7_pw = await bcrypt.hash('Patricia', saltRounds);
+	const user7 = await users.addUser('patricia', 'patricia@gmail.com', '297-829-9134', user7_pw);
+	const house7 = await houses.addHouse(
+		'201 Washington St, Downtown, NJ 07302', 
+		'Shared Room Downtown', 
+		user7._id, 40.7140661, -74.038208, 'Shared Room', 695, ''
+	);
+
+	const user8_pw = await bcrypt.hash('Charlaine', saltRounds);
+	const user8 = await users.addUser('charlaine', 'charlaine@gmail.com', '425-545-3164', user8_pw);
+	const house8 = await houses.addHouse(
+		'382 Van Horne St, Jersey City, NJ 07304', 
+		'Minutes to Manhattan & Jersey Shore', 
+		user8._id, 40.710149, -74.060111, 'Entire home/apt', 3180, ''
+	);
+	const house9 = await houses.addHouse(
+		'7 Monitor St, Jersey City, NJ 07304', 
+		'Minutes to Manhattan and NJ Shore', 
+		user8._id, 40.7165239, -74.0580937, 'Entire home/apt', 2900, ''
+	);
+
+	const user9_pw = await bcrypt.hash('Bozena', saltRounds);
+	const user9 = await users.addUser('bozena', 'bozena@gmail.com', '201-391-2184', user9_pw);
+	const house10 = await houses.addHouse(
+		'138 Leonard St, Jersey City, NJ 07307', 
+		'Cozy Shared Room / Free WiFi / 25 Min To Time Sq', 
+		user9._id, 40.757235, -74.049168, 'Shared Room', 500, ''
+	);
+
+	const user10_pw = await bcrypt.hash('Merlin', saltRounds);
+	const user10 = await users.addUser('merlin', 'merlin@gmail.com', '461-623-5141', user10_pw);
+	const house11 = await houses.addHouse(
+		'175 Van Horne St, Jersey City, NJ 07304', 
+		'Private room with own bathroom close to NYC', 
+		user10._id, 40.7118974, -74.0652272, 'Private Room', 1190, ''
+	);
+	const house12 = await houses.addHouse(
+		'291 Halladay St, Jersey City, NJ 07304', 
+		'Large Room with private bathroom 20 min to Manhattan', 
+		user10._id, 40.7125955, -74.0628522, 'Private Room', 1000, ''
+	);
+
+	const user11_pw = await bcrypt.hash('Armanda', saltRounds);
+	const user11 = await users.addUser('armanda', 'armanda@gmail.com', '341-227-7532', user11_pw);
+	const house13 = await houses.addHouse(
+		'11 Saddlewood Ct, Downtown, NJ 07302', 
+		'1500sf 1BR Loft: Open Plan+NYC View', 
+		user11._id, 40.721962, -74.041651, 'Entire home/apt', 4200, ''
+	);
+
+	const user12_pw = await bcrypt.hash('Micheal', saltRounds);
+	const user12 = await users.addUser('micheal', 'micheal@gmail.com', '168-845-5686', user12_pw);
+	const house14 = await houses.addHouse(
+		'321 8th St, Jersey City, NJ 07302', 
+		'Your Own Apartment Two Stops Away', 
+		user12._id, 40.7269078, -74.0479109, 'Entire home/apt', 3600, ''
+	);
+
+	const user13_pw = await bcrypt.hash('Christopher', saltRounds);
+	const user13 = await users.addUser('christopher', 'christopher@gmail.com', '723-611-4806', user13_pw);
+	const house15 = await houses.addHouse(
+		'201 Newark Ave, Downtown, NJ 07302', 
+		'LARGE SUNNY ROOM 15 MIN to NYC!', 
+		user13._id, 40.7215897, -74.0464906, 'Private Room', 1500, ''
+	);
+
+	const user14_pw = await bcrypt.hash('Robert', saltRounds);
+	const user14 = await users.addUser('robert', 'robert@gmail.com', '272-470-8898', user14_pw);
+	const house16 = await houses.addHouse(
+		'410 Marin Blvd, Downtown, NJ 07302', 
+		'Lavish 2BR in Jersey City + Great Amenities', 
+		user14._id, 40.7241331, -74.0374535, 'Hotel Room', 5000, ''
+	);
+
+	const user15_pw = await bcrypt.hash('Faisal', saltRounds);
+	const user15 = await users.addUser('faisal', 'faisal@gmail.com', '318-005-2442', user15_pw);
+	const house17 = await houses.addHouse(
+		'609 Grove St, Jersey City, NJ 07310', 
+		'Shared 2 bedrooms/ male only/ 6 min drive from Manhattan', 
+		user15._id, 40.7325677, -74.0411308, 'Shared Room', 880, ''
+	);
 
 	console.log('Done seeding database');
-
 	await db.serverConfig.close();
 }
 
