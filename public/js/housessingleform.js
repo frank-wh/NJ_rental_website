dcf6adedcaedc1ef4d726f6a7b3ba01772a19189
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-07-13 21:14:18 -0400

    Add files via upload

--- public/js/housessingleform.js
+++ public/js/housessingleform.js
@@ -56,6 +56,23 @@
         });
     });
 
+    $('#deleteSubmit').on('click', function(event) {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Are you sure to delete?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                $('#housesingledelete').submit();
+            }
+        })
+    });
+
     $('#commentSubmit').on('click', function(event) {
         event.preventDefault();
         if($('#currentUser').html() === undefined){
--- public/js/userssingleform.js
+++ public/js/userssingleform.js
@@ -1,13 +1,30 @@
 $('form').submit(function(event) {
 	event.preventDefault();
-	$(this).addClass('toRemove');
-	$.ajax({
-		url: $(this).attr('action'),
-		type: 'DELETE',
-		success: function(res) {
-			$('.toRemove').closest('li').fadeOut(500, function() {
-				$(this).remove();
+	const form = $(this);
+	const formUrl = form.attr('action');
+	form.toggleClass('toRemove');
+
+	Swal.fire({
+		title: 'Are you sure to delete?',
+		text: "You won't be able to revert this!",
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Yes, delete it!'
+	}).then((result) => {
+		if (result.value) {
+			$.ajax({
+				url: formUrl,
+				type: 'DELETE',
+				success: function(res) {
+					$('.toRemove').closest('li').fadeOut(500, function() {
+						$(this).remove();
+					});
+				}
 			});
+		} else {
+			form.toggleClass('toRemove');
 		}
-	});
+	})
 });
