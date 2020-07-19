5bf8ae70577c0a9119819bc153d8f486d403ae36
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-07-19 12:38:29 -0400

    Add files via upload

--- public/js/loginform.js
+++ public/js/loginform.js
@@ -1,18 +1,16 @@
-if ($('#signUpServerError').find('div').length > 0 || $('#signInServerError').find('div').length > 0) {
-    $('#loginModal').modal('show');
-}
+window.onload = function() {
+    if (localStorage.getItem('hasUser')) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Welcome, ' + $('#currentUser').html(),
+            showConfirmButton: false,
+            timer: 1500
+        })
+        localStorage.removeItem('hasUser');
+    }
+};
 
-if ($('#signUpServerError').find('div').length > 0) {
-    $('#signInDiv').hide();
-    $('#signUpDiv').show();
-
-    $('#signInDivButton').toggleClass('active');
-    $('#signUpDivButton').toggleClass('active');
-}
-else {
-    $('#signInDiv').show();
-    $('#signUpDiv').hide();
-}
+$('#signUpDiv').hide();
 
 $('#signInDivButton').on('click', (event) => {
     event.preventDefault();
@@ -55,11 +53,27 @@ $('#signInButton').on('click', (event) => {
     $('#signInClientError').hide();
     
     if (!$('#signInInfo').val() || !$('#signInPassword').val()) {
-        $('#signInClientError').html('Error: Please check that you\'ve entered username/email and password!');
+        $('#signInClientError').html('Please check that you\'ve entered username/email and password!');
         $('#signInClientError').show();
     }
     else {
-        $('#signInForm').submit();
+        $.ajax({
+            url: $('#signInForm').attr('action'),
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                loginInfo: $('#signInInfo').val(),
+                password: $('#signInPassword').val()
+            })
+        })
+        .done(() => {
+            localStorage.setItem('hasUser', 'success');
+            location.reload();
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+			$('#signInServerError').html(jqXHR.responseText);
+			$('#signInServerError').show();
+		});
     }
 });
 
@@ -83,17 +97,17 @@ $('#signUpButton').on('click', (event) => {
     // check username
     if (!$('#signUpName').val()) {
         hasError = true;
-        $('#signUpNameError').html('Error: Please check that you\'ve entered an username');
+        $('#signUpNameError').html('Please check that you\'ve entered an username');
         $('#signUpNameError').show();
     } 
     else if ($('#signUpName').val().length < 6) {
         hasError = true;
-        $('#signUpNameError').html('Error: Username must contain at least six characters!');
+        $('#signUpNameError').html('Username must contain at least six characters!');
         $('#signUpNameError').show();
     } 
     else if ($('#signUpName').val().length > 16) {
         hasError = true;
-        $('#signUpNameError').html('Error: Username must contain no more than sixteen characters!');
+        $('#signUpNameError').html('Username must contain no more than sixteen characters!');
         $('#signUpNameError').show();
     }
 
@@ -102,32 +116,32 @@ $('#signUpButton').on('click', (event) => {
     const passwordLowerCase = $('#signUpPassword').val().toLowerCase();
     if (!$('#signUpPassword').val() || !$('#signUpPassword2').val() || $('#signUpPassword').val() !== $('#signUpPassword2').val()) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Please check that you\'ve entered and confirmed your password!');
+        $('#signUpPasswordError').html('Please check that you\'ve entered and confirmed your password!');
         $('#signUpPasswordError').show();
     }
     else if ($('#signUpPassword').val() === $('#signUpName').val()) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Please check that your password is different from your username!');
+        $('#signUpPasswordError').html('Please check that your password is different from your username!');
         $('#signUpPasswordError').show();
     }
     else if (!passwordRegex.test($('#signUpPassword').val())) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Password must contain only letters, numbers and underscores!');
+        $('#signUpPasswordError').html('Password must contain only letters, numbers and underscores!');
         $('#signUpPasswordError').show();
     } 
     else if ($('#signUpPassword').val().length < 6) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Password must contain at least six letters!');
+        $('#signUpPasswordError').html('Password must contain at least six letters!');
         $('#signUpPasswordError').show();
     } 
     else if ($('#signUpPassword').val().length > 16) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Password must contain no more than sixteen letters!');
+        $('#signUpPasswordError').html('Password must contain no more than sixteen letters!');
         $('#signUpPasswordError').show();
     } 
     else if ($('#signUpPassword').val() === passwordLowerCase) {
         hasError = true;
-        $('#signUpPasswordError').html('Error: Password must contain at least one uppercase letter (A-Z)!');
+        $('#signUpPasswordError').html('Password must contain at least one uppercase letter (A-Z)!');
         $('#signUpPasswordError').show();
     }
 
@@ -135,12 +149,12 @@ $('#signUpButton').on('click', (event) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!$('#signUpEmail').val()) {
         hasError = true;
-        $('#signUpEmailError').html('Error: Please check that you\'ve entered an email');
+        $('#signUpEmailError').html('Please check that you\'ve entered an email');
         $('#signUpEmailError').show();
     }
     else if (!emailRegex.test($('#signUpEmail').val())) {
         hasError = true;
-        $('#signUpEmailError').html('Error: Please make sure you\'ve entered a valid email');
+        $('#signUpEmailError').html('Please make sure you\'ve entered a valid email');
         $('#signUpEmailError').show();
     }
 
@@ -153,16 +167,34 @@ $('#signUpButton').on('click', (event) => {
     $('#signUpPhoneNumber').val(phoneArr.join('-'));
     if ($('#signUpPhoneNumber').val() === '--') {
         hasError = true;
-        $('#signUpPhoneError').html('Error: Please check that you\'ve entered a phone number');
+        $('#signUpPhoneError').html('Please check that you\'ve entered a phone number');
         $('#signUpPhoneError').show();
     }
     else if (!phoneRegex.test($('#signUpPhoneNumber').val())) {
         hasError = true;
-        $('#signUpPhoneError').html('Error: Please make sure you\'ve entered a valid phone number');
+        $('#signUpPhoneError').html('Please make sure you\'ve entered a valid phone number');
         $('#signUpPhoneError').show();
     }
 
     if (!hasError) {
-        $('#signUpForm').submit();
+        $.ajax({
+            url: $('#signUpForm').attr('action'),
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                username: $('#signUpName').val(),
+                password: $('#signUpPassword').val(),
+                email: $('#signUpEmail').val(),
+                phoneNumber: $('#signUpPhoneNumber').val()
+            })
+        })
+        .done(() => {
+            localStorage.setItem('hasUser', 'success');
+            location.reload();
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+			$('#signUpServerError').html(jqXHR.responseText);
+			$('#signUpServerError').show();
+		});
     }
 });
