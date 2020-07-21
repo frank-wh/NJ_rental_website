ec09716115e93357df6a8f0b21bc6313b89148a1
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-07-21 11:06:52 -0400

    Add files via upload

--- public/js/housesnewform.js
+++ public/js/housesnewform.js
@@ -1,51 +1,47 @@
-let latlng;
-let map;
+$('#nextBtn').hide();
+$('#backBtn').hide();
+$('#nextDiv').hide();
 
-$('#back').on('click', () => {
-    $('#addrDiv').show();
-    $('#judgeDiv').addClass('d-none');
-    $('#nextDiv').addClass('d-none');
+$('#backBtn').on('click', () => {
+    $('#address').prop('readonly', false);
+    $('#nextBtn, #backBtn').fadeOut(400, () => {
+        $('#checkBtn').fadeIn();
+    });
     map = new google.maps.Map(document.getElementById('houseNewMap'), {
         zoom: 12,
-        center: {lat: 40.728157, lng: -74.077644} // jersey city lat lng
+        center: {lat: 40.728157, lng: -74.077644}
     });
 });
-$('#next').on('click', () => {
-    $('#addrDiv').hide();
-    $('#judgeDiv').addClass('d-none');
-    $('#houseNewMap').hide();
-    $('#nextDiv').removeClass('d-none');
-});
-$('#backToAddr').on('click', () => {
-    $('#houseNewFormSubmitError').hide();
-    $('#houseNewFormError').hide();
-    $('#addrDiv').show();
-    $('#judgeDiv').addClass('d-none');
-    $('#houseNewMap').show();
-    $('#nextDiv').addClass('d-none');
-    map = new google.maps.Map(document.getElementById('houseNewMap'), {
-        zoom: 12,
-        center: {lat: 40.728157, lng: -74.077644} // jersey city lat lng
+$('#nextBtn').on('click', () => {
+    $('#address').prop('readonly', true);
+    $('#houseNewMap, #nextBtn, #backBtn').fadeOut(400, () => {
+        $('#nextDiv').fadeIn();
     });
+});
+$('#backToAddrBtn').on('click', () => {
+    $('#checkBtn').hide();
+    $('#nextDiv').fadeOut(400, () => {
+        $('#houseNewMap, #nextBtn, #backBtn').fadeIn();
+	});
 });
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('houseNewMap'), {
         zoom: 12,
-        center: {lat: 40.728157, lng: -74.077644} // jersey city lat lng
+        center: {lat: 40.728157, lng: -74.077644}
     });
     const geocoder = new google.maps.Geocoder();
-    $('#check').on('click', () => {
+    $('#checkBtn').on('click', () => {
         geocodeAddress(geocoder, map);
     });
 }
 
 function geocodeAddress(geocoder, resultsMap) {
-    if($('#address').val() !== ""){
+    if ($('#address').val() !== "") {
         geocoder.geocode({'address': $('#address').val()}, (results, status) => {
             if (status === 'OK') {
                 latlng = results[0].geometry.location;
-                if(latlng.lat()>=40.77 || latlng.lat()<=40.665 || latlng.lng() >=-74.025 || latlng.lng() <=-74.11){
+                if (latlng.lat() >= 40.77 || latlng.lat() <= 40.665 || latlng.lng() >= -74.025 || latlng.lng() <= -74.11) {
                     latlng = undefined;
                     return Swal.fire({
                         icon: 'warning',
@@ -60,9 +56,12 @@ function geocodeAddress(geocoder, resultsMap) {
                     map: resultsMap,
                     position: latlng
                 });
-                $('#judgeDiv').removeClass('d-none');
-                $('#addrDiv').hide();
-            } else {
+                $('#address').prop('readonly', true);
+                $('#checkBtn').fadeOut(400, () => {
+                    $('#nextBtn, #backBtn').fadeIn();
+                });
+            } 
+            else {
                 return Swal.fire({
                     icon: 'warning',
                     title: 'Geocode was not successful for the following reason: ' + status,
@@ -74,34 +73,52 @@ function geocodeAddress(geocoder, resultsMap) {
     }
 }
 
-$('button[type=submit]').on('click', (event) => {
+$('#postSubmitBtn').on('click', (event) => {
     event.preventDefault();
-
-    $('#houseNewFormSubmitError').hide();
-    $('#houseNewFormError').hide();
-
-    if(latlng !== undefined){
+    $('#postFormError').hide();
+    $('#postFormServerError').hide();
+    if (latlng !== undefined) {
         $('#lat').val(latlng.lat());
         $('#lng').val(latlng.lng());
     }
-    
     const arr = $('#image').val().split('.');
-    if(!$('#statement').val() || !$('#type').val() || !$('#price').val() || !$('#image').val()){
-        $('#houseNewFormSubmitError').html('Error: Please make sure that every field is filled and an image is choosen!');
-        $('#houseNewFormSubmitError').show();
+    if (!$('#statement').val() || !$('#type').val() || !$('#price').val() || !$('#image').val()) {
+        $('#postFormError').html('Please make sure that every field is filled');
+        $('#postFormError').show();
         return;
     }
-    else if(arr[arr.length - 1] !== 'jpg' && arr[arr.length - 1] !== 'png'){
-        $('#houseNewFormSubmitError').html('Error: The file could only be image (jpg, jpeg, png) format!');
-        $('#houseNewFormSubmitError').show();
+    else if (arr[arr.length - 1] !== 'jpg' && arr[arr.length - 1] !== 'png') {
+        $('#postFormError').html('The file could only be image (jpg, jpeg, png) format!');
+        $('#postFormError').show();
         return;
     }
-
-    if($('#price').val() < 1){
+    if ($('#price').val() < 1) {
         $('#price').val(1);
     }
-    else if($('#price').val() > 9999){
+    else if ($('#price').val() > 9999) {
         $('#price').val(9999);
     }
-    $('#housesNewForm').submit();
+
+    let formdata = new FormData();
+    formdata.append('image', $('#image')[0].files[0]);
+    formdata.append('address', $('#address').val());
+    formdata.append('statement', $('#statement').val());
+    formdata.append('lat', $('#lat').val());
+    formdata.append('lng', $('#lng').val());
+    formdata.append('roomType', $('#type').val());
+    formdata.append('price', $('#price').val());
+    $.ajax({
+        url: $('#postForm').attr('action'),
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: formdata
+    })
+    .done((res) => {
+        window.location.href = res.redirectURL;
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+        $('#postFormServerError').html(jqXHR.responseText);
+        $('#postFormServerError').show();
+    });
 });
--- public/js/userssingleform.js
+++ public/js/userssingleform.js
@@ -1,13 +1,25 @@
 $('#editCancel').hide();
 $('#editDiv').hide();
+$('#postDiv').hide();
+let isEdit = false;
 
 $('#editBtn').on('click', (event) => {
 	event.preventDefault();
 	$('#editCancel').show();
 	$('#editAndPost').hide();
-
-	$('#usersingleinfo').fadeOut(400, function() {
+    isEdit = true;
+	$('#usersingleinfo').fadeOut(400, () => {
 		$('#editDiv').fadeIn();
+	});
+});
+
+$('#postBtn').on('click', (event) => {
+	event.preventDefault();
+	$('#editCancel').show();
+	$('#editAndPost').hide();
+    isEdit = false;
+	$('#usersingleinfo').fadeOut(400, () => {
+        $('#postDiv').fadeIn();
 	});
 });
 
@@ -15,13 +27,19 @@ $('#editCancelBtn').on('click', (event) => {
 	event.preventDefault();
 	$('#editCancel').hide();
 	$('#editAndPost').show();
-
-	$('#editDiv').fadeOut(400, function() {
-		$('#usersingleinfo').fadeIn();
-	});
+    if (isEdit) {
+        $('#editDiv').fadeOut(400, () => {
+            $('#usersingleinfo').fadeIn();
+        });
+    }
+    else {
+        $('#postDiv').fadeOut(400, () => {
+            $('#usersingleinfo').fadeIn();
+        });
+    }
 });
 
-$('.infoForm').on('click', function(event) {
+$('.infoListForm').on('click', function(event) {
 	event.preventDefault();
 	const form = $(this);
 	const formUrl = form.attr('action');
@@ -32,7 +50,7 @@ $('.infoForm').on('click', function(event) {
 		text: "You won't be able to revert this!",
 		icon: 'warning',
 		showCancelButton: true,
-		confirmButtonColor: '#3085d6',
+		confirmButtonColor: '#28a745',
 		cancelButtonColor: '#d33',
 		confirmButtonText: 'Yes, delete it!'
 	})
@@ -54,123 +72,18 @@ $('.infoForm').on('click', function(event) {
 	})
 });
 
-/***************************** edit form *****************************/
-$('.phoneNumber').keyup(function () {
-    if (this.value.length == this.maxLength) {
-        $(this).next().next().focus();
-    }
-});
-
-$('#editSubmitBtn').on('click', (event) => {
-    event.preventDefault();
-
-    let hasError = false;
-    $('#emptyError').hide();
-    $('#passwordError').hide();
-    $('#emailError').hide();
-    $('#phoneError').hide();
-    $('#editServerError').hide();
-
-    let phoneArr = [];
-    $('.phoneNumber').each(function() {
-        phoneArr.push($(this).val());
-    })
-    $('#phoneNumber').val(phoneArr.join('-'));
-
-    // check empty
-    if (!$('#password').val() && !$('#email').val() && $('#phoneNumber').val() === "--") {
-        hasError = true;
-        $('#emptyError').html('Please enter something to edit your profile!');
-        $('#emptyError').show();
-    }
-
-    // check password
-    if ($('#password').val()) {
-        const passwordRegex = /^\w+$/;
-        const passwordLowerCase = $('#password').val().toLowerCase();
-        if ($('#password').val() !== $('#password2').val()) {
-            hasError = true;
-            $('#passwordError').html('Please check that you\'ve entered and confirmed your password!');
-            $('#passwordError').show();
+$('.infoForm').on('click', function(event) {
+	event.preventDefault();
+	const form = $(this);
+	const formUrl = form.attr('action');
+	form.toggleClass('toRemove');
+    $.ajax({
+        url: formUrl,
+        type: 'DELETE',
+        success: () => {
+            $('.toRemove').closest('li').fadeOut(500, function() {
+                $(this).remove();
+            });
         }
-        else if ($('#password').val() === $('#username').val()) {
-            hasError = true;
-            $('#passwordError').html('Please check that your password is different from your username!');
-            $('#passwordError').show();
-        }
-        else if (!passwordRegex.test($('#password').val())) {
-            hasError = true;
-            $('#passwordError').html('Password must contain only letters, numbers and underscores!');
-            $('#passwordError').show();
-        } 
-        else if ($('#password').val().length < 6) {
-            hasError = true;
-            $('#passwordError').html('Password must contain at least six characters!');
-            $('#passwordError').show();
-        }
-        else if ($('#password').val().length > 16) {
-            hasError = true;
-            $('#passwordError').html('Password must contain no more than sixteen letters!');
-            $('#passwordError').show();
-        } 
-        else if ($('#password').val() === passwordLowerCase) {
-            hasError = true;
-            $('#passwordError').html('Password must contain at least one uppercase letter (A-Z)!');
-            $('#passwordError').show();
-        }
-    }
-
-    // check email
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
-    if ($('#email').val() && !emailRegex.test($('#email').val())) {
-        hasError = true;
-        $('#emailError').html('Please make sure you\'ve entered a valid email');
-        $('#emailError').show();
-    }
-
-    // check phone number
-    const phoneRegex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
-    if ($('#phoneNumber').val() === '--') {
-        $('#phoneNumber').val('');
-    }
-    if ($('#phoneNumber').val() && !phoneRegex.test($('#phoneNumber').val())) {
-        hasError = true;
-        $('#phoneError').html('Please make sure you\'ve entered a valid phone number');
-        $('#phoneError').show();
-    }
-
-    if(!hasError){
-		$.ajax({
-			url: $('#editForm').attr('action'),
-			type: 'PUT',
-			contentType: "application/json",
-			data: JSON.stringify({
-				password: $('#password').val(),
-				email: $('#email').val(),
-				phoneNumber: $('#phoneNumber').val()
-			})
-		})
-		.done(() => {
-			if ($('#email').val()) {
-				$('#emailSpan').text($('#email').val());
-				$('#email').val('');
-			}
-			else if ($('#phoneNumber').val()) {
-				$('#phoneSpan').text($('#phoneNumber').val());
-				$('#phoneNumberPart1').val('');
-				$('#phoneNumberPart2').val('');
-				$('#phoneNumberPart3').val('');
-			}
-			Swal.fire({
-				icon: 'success',
-				title: 'Your profile has been updated',
-				showConfirmButton: false,
-				timer: 1500
-			})
-		})
-		.fail((jqXHR, textStatus, errorThrown) => {
-			$('#editServerError').html(jqXHR.responseText);
-			$('#editServerError').show();
-		});
-    }
+    });
 });
